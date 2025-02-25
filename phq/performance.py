df29362e8fae8368afc5f7d9f100234758664380
XipingFu <57967798+XipingFu@users.noreply.github.com>
2025-02-26 09:21:11 +1300

    Update warning message when there are days with small positive demand values

--- phq/performance.py
+++ phq/performance.py
@@ -9,34 +9,29 @@ log = logging.getLogger(__name__)
 DEMAND_PERCENT_THRESHOLD = 5
 
 
-def get_forecast_metrics(
-    results: pd.DataFrame, ignore_low_demand: bool = True
-) -> float:
+def get_forecast_metrics(results: pd.DataFrame) -> float:
     results = results.sort_values("date")
 
-    if ignore_low_demand:
-        results["demand_percent"] = results["demand"] / results["demand"].mean() * 100
-        low_demand_days = results[
-            (results["demand"] > 0)
-            & (results["demand_percent"] < DEMAND_PERCENT_THRESHOLD)
-        ]
+    # Check if there are small postive demand values
+    results["demand_percent"] = results["demand"] / results["demand"].mean() * 100
+    low_demand_days = results[
+        (results["demand"] > 0)
+        & (results["demand_percent"] < DEMAND_PERCENT_THRESHOLD)
+    ]
 
-        if len(low_demand_days) > 0:
-            log.warning(
-                f"There are demand values that are less than {DEMAND_PERCENT_THRESHOLD}% of the mean demand.",
-                "Forecasting results for days with these small demand values and the days following them are skipped.",
-            )
-            date_upper = results[results["demand_percent"] < DEMAND_PERCENT_THRESHOLD][
-                "date"
-            ].values[0]
-            results = results[results["date"] < date_upper]
+    # Output warning message if there are days with small demand values
+    if len(low_demand_days) > 0:
+        log.warning(
+            f"There are positive demand values that are less than {DEMAND_PERCENT_THRESHOLD}% of the mean demand. "
+            "This might lead to extremely large MAPE. "
+            "Please check whether these small demand values represent actual daily demand.",
+        )
 
-    # skip where demand or forecast is null or days with missing demand values
+    # Skip where demand or forecast is null or days with missing demand values
     valid = (
         results["demand"].notnull()
         & results["forecast"].notnull()
-        & results["is_imputed_demand"]
-        == 0
+        & (results["is_imputed_demand"]== 0)
     )
     results = results[valid]
 
